272b3c4c3cf82538f75dfec88557cdc545526b3f
AiUU <aiuuchan1120@gmail.com>
2024-07-26 01:32:14 +0800

    Generated cube texture

---
+++ Assets/Editor/RenderToCubeMapWindow.cs
@@ -0,0 +1,42 @@
+using UnityEditor;
+using UnityEngine;
+
+namespace Editor
+{
+    public sealed class RenderToCubeMapWindow : EditorWindow
+    {
+        private GameObject m_Obj;
+
+        private Cubemap m_Cubemap;
+
+        [MenuItem("立方体纹理动态生成/打开生成窗口")]
+        static void OpenWindow()
+        {
+            var window = EditorWindow.GetWindow<RenderToCubeMapWindow>("立方体纹理生成窗口");
+            window.Show();
+        }
+
+        private void OnGUI()
+        {
+            GUILayout.Label("关联对应位置对象");
+            m_Obj = EditorGUILayout.ObjectField(m_Obj, typeof(GameObject), true) as GameObject;
+            GUILayout.Label("关联对应立方体纹理");
+            m_Cubemap = EditorGUILayout.ObjectField(m_Cubemap, typeof(Cubemap), false) as Cubemap;
+            if (GUILayout.Button("生成立方体纹理"))
+            {
+                if (m_Obj == null || m_Cubemap == null)
+                {
+                    EditorUtility.DisplayDialog("提醒", "请先关联对应对象和立方体贴图", "确认");
+                    return;
+                }
+                // 动态生成立方体纹理
+                var tmpObj = new GameObject("临时对象");
+                tmpObj.transform.position = m_Obj.transform.position;
+                var camera = tmpObj.AddComponent<Camera>();
+                // 关键方法 可以马上生成6张2D纹理贴图
+                camera.RenderToCubemap(m_Cubemap);
+                DestroyImmediate(tmpObj);
+            }
+        }
+    }
+}
